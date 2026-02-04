272f897dd9e57d94c82e27d64a559781751e6b69
GregorySmok <g.smokovdin@yandex.ru>
2026-02-04 21:09:00 +0300

    bugfix

--- handlers/user_handlers/start_handler.py
+++ handlers/user_handlers/start_handler.py
@@ -8,6 +8,6 @@ def setup(router):
     @router.message(Command(commands=["start"]))
     async def start_handler(message: Message, state: FSMContext):
         await shared.bot.send_message(message.from_user.id, text="💘 Анонимные признания\n\n"
-        "Я опубликую твою валентинку в наш канал.\n"
+        "Я опубликую твою валентинку в наш канал\n"
         "Напиши свою валентинку:")
         await state.set_state(States.typing_message)
